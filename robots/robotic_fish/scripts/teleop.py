8ab7766aef009289d71a14eaae5826d2ffc9a0b2
Guitarwang01 <guitarwang01@gmail.com>
2024-12-10 22:33:02 +0900

    [double servo] Developed the first demo of double servo.

--- robots/robotic_fish/scripts/teleop.py
+++ robots/robotic_fish/scripts/teleop.py
@@ -7,6 +7,7 @@ import numpy as np
 import rospy
 import math
 from spinal.msg import ServoControlCmd
+from spinal.msg import ServoStates
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
 
@@ -16,18 +17,48 @@ class Teleop():
         self.joy_dead_zone = rospy.get_param('~joy_dead_zone', 0.1)
         self.vel_rate = rospy.get_param('~vel_rate', 250.0)
         self.max_val = rospy.get_param('~max_val', 250)
-        self.keep_vel = rospy.get_param('~keep_vel', 0.0)
+        self.keep_vel = rospy.get_param('~keep_vel', [0.0, 0.0])
+        self.servo_equ_angles = rospy.get_param('~servos_angle', [0, 0, 0])
+        self.servo_cmd = rospy.get_param('~servo_cmd', [0, 0, 1024.0])
 
         self.fin_angle_rate = rospy.get_param('fin_angle_rate', 1024.0)
 
         self.joy_sub = rospy.Subscriber('/joy', Joy, self._joyCallback)
         self.twist_sub = rospy.Subscriber('/cmd_vel', Twist, self._twistCallback)
+        self.servo_pos_sub = rospy.Subscriber('/servo/states', ServoStates, self._servoStateCallback)
 
         self.cmd_pub = rospy.Publisher('/servo/target_states', ServoControlCmd, queue_size=1)
 
 
+    def _servoStateCallback(self, msg):
+        self.servo_equ_angles = [msg.servos[0].angle % 4096, msg.servos[1].angle % 4096, msg.servos[2].angle]
+        #rospy.loginfo("servo 0 at angle %s", msg.servos[0].angle)
+
+    def servo_sync(self):
+        self.servo_cmd[0] = 0.0
+        self.servo_cmd[1] = 0.0
+        if self.servo_equ_angles[0] - 2048 < -200:
+            self.servo_cmd[0] = self.max_val
+        elif self.servo_equ_angles[0] - 2048 < -70:
+            self.servo_cmd[0] = 0.3 * self.max_val
+        elif self.servo_equ_angles[0] - 2048 > 200:
+            self.servo_cmd[0] = -self.max_val
+        elif self.servo_equ_angles[0] - 2048 > 70:
+            self.servo_cmd[0] = -0.3 * self.max_val
+
+        if self.servo_equ_angles[1] - 2048 < -200:
+            self.servo_cmd[1] = self.max_val
+        elif self.servo_equ_angles[1] - 2048 < -70:
+            self.servo_cmd[1] = 0.3 * self.max_val
+        elif self.servo_equ_angles[1] - 2048 > 200:
+            self.servo_cmd[1] = -self.max_val
+        elif self.servo_equ_angles[1] - 2048 > 70:
+            self.servo_cmd[1] = -0.3 * self.max_val
+
+
     def _joyCallback(self, msg):
 
+        # forward
         forward_vel = msg.axes[1]
         if math.fabs(forward_vel) < self.joy_dead_zone:
             forward_vel = 0
@@ -38,25 +69,45 @@ class Teleop():
         if forward_val < -self.max_val:
             forward_val = -self.max_val
 
-        # TODO1
-        if msg.buttons[5] == 1: #R2
-            forward_val = self.keep_vel
-        else:
-            self.keep_vel = forward_val
+        self.servo_cmd[0] = forward_val
+        self.servo_cmd[1] = forward_val
 
-        #TODO2
-        fin_angle_ctrl = msg.axes[5]
+        # hold servo 1 and only control servo 0
+        if msg.buttons[0] == 1: # square
+            self.servo_cmd[1] = 0
+
+        # only control servo 1 with L2 & R2
+        if msg.buttons[6] == 1: #L2
+            vel_0 = 0.5 * (msg.axes[3] - 1.0)
+            if math.fabs(vel_0) > self.joy_dead_zone:
+                self.servo_cmd[1] = vel_0 * self.vel_rate
+        if msg.buttons[7] == 1: #R2
+            vel_0 = -0.5 * (msg.axes[4] - 1.0)
+            if math.fabs(vel_0) > self.joy_dead_zone:
+                self.servo_cmd[1] = vel_0 * self.vel_rate
+
+        #TODO2: fin control
+        fin_angle_ctrl = msg.axes[5] # stick_r vertical
         fin_angle = (-1)*fin_angle_ctrl * self.fin_angle_rate + 2048.0
-        # TODO1: push L2 or R2 buttons to give constant forward vel
+        self.servo_cmd[2] = fin_angle
 
-        # TODO2: use right joystick to control the fin
+        # TODO1: Speed Keep
+        if msg.buttons[5] == 1: #R2
+            self.servo_cmd[0] = self.keep_vel[0]
+            self.servo_cmd[1] = self.keep_vel[1]
+        else:
+            self.keep_vel = [self.servo_cmd[0], self.servo_cmd[1]]
+
+        # sync the two servos to middle position
+        if msg.buttons[1] == 1: # X
+            self.servo_sync()
 
         # motor_vel = np.array([int(forward_vel), int(forward_val)], dtype=np.int16)
         msg_0 = ServoControlCmd()
         msg_0.index = [0,1,2]
-        msg_0.cmd.append(int(forward_val))
-        msg_0.cmd.append(int(forward_val))
-        msg_0.cmd.append(int(fin_angle))
+        msg_0.cmd.append(int(self.servo_cmd[0]))
+        msg_0.cmd.append(int(self.servo_cmd[1]))
+        msg_0.cmd.append(int(self.servo_cmd[2]))
         self.cmd_pub.publish(msg_0)
 
 
